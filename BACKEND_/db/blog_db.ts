6430503dd86d844f0b5851d4180abab500af5b3b
niyibi250 <122937285+niyibi250@users.noreply.github.com>
2024-03-20 20:28:55 +0200

    Delete BACKEND_ directory

--- BACKEND_/db/blog_db.ts
+++
@@ -1,9 +0,0 @@
-import { promises } from "dns";
-import mongoose from "mongoose";
-
-const connection_string:string='mongodb://localhost:27017/MY_BLAND_DB'
-
-export const connectDB= async function():Promise<void>
-{
-    await mongoose.connect(connection_string)
-}
